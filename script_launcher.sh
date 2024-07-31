5dc04ef6820382c1ad2017c1c07e2d5abf0a952c
Matias Ceau <matias@ceau.net>
2024-07-31 20:01:09 +0200

    52 change(s) from matias@karjala

--- script_launcher.sh
+++ script_launcher.sh
@@ -2,10 +2,22 @@
 
 #INFO:# "Run scripts with fzf"
 
-fd '' -tx "$SCRIPTS" |
-	xargs -I {} basename {} |
+preview_cmd_docs='bat -lmd $SCRIPTS/docs/scripts/{}.md --color=always --style=grid'
+preview_cmd_source='bat --style=numbers --color=always $(fd {} -tx $SCRIPTS | rg '{}$')'
+
+fd '' -tx --format '{/}' "$SCRIPTS" |
 	improved-fzfmenu.sh \
-		--preview "bat -lmd $SCRIPTS/docs/scripts/{}.md --color=always --style=grid" \
+		--algo=v1 \
+		--preview "$preview_cmd_docs" \
 		--preview-window='70%' \
-		--bind "enter:become(bash -c {})"
-#	xargs -I {} bash -c "{}" &
+		--bind "enter:become(bash -c {})" \
+		--bind "alt-e:become(nvim \$(which {}))" \
+		--bind alt-s:change-preview@"$preview_cmd_source"@ \
+		--bind alt-d:change-preview@"$preview_cmd_docs"@
+
+#fd '' -tx --format '{/}' "$SCRIPTS" |
+#	improved-fzfmenu.sh \
+#		--preview "bat -lmd $SCRIPTS/docs/scripts/{}.md --color=always --style=grid" \
+#		--preview-window='70%' \
+#		--bind "enter:become(bash -c {})"
+##	xargs -I {} bash -c "{}" &
