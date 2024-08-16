9a26a4384d3bb6d7703d1f6843830b684c92ee47
Matias Ceau <matias@ceau.net>
2024-08-16 21:33:18 +0200

    1 change(s) from matias@karjala

---
+++ qtile_windows.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# qtile cmd-obj -o root -f windows -> get all window infos (apparently in json)
+# qtile cmd-obj -o root -f commands -> get all commands
+# see: https://docs.qtile.org/en/v0.28.1/manual/commands/shell/qtile-cmd.html
+echo
