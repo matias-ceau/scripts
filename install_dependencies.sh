83925a7d545a3db18c9ace1d6eab32b5f2865e22
Matias Ceau <matias@ceau.net>
2024-09-06 21:13:02 +0200

    1 change(s) from matias@karjala

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-sudo pacman -S \
-    git \
-    fd \
-    bat \
-    fzf \
-    ripgrep \
-    neovim \
-    chezmoi \
-    dmenu \
-    findutils \
-    rofi \
-    dunst \
-    python-pandas \
-    python-colorama
+paru -S --needed - < <(
+    cat <<EOF
+git
+fd
+bat
+fzf
+ripgrep
+neovim
+chezmoi
+dmenu
+findutils
+rofi
+dunst
+python-pandas
+python-colorama
+light
+xdotool
+EOF
+)
