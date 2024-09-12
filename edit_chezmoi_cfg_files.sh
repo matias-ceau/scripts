0e23456e16e231a27a1b231b2151aff719b60c68
Matias Ceau <matias@ceau.net>
2024-09-12 18:25:12 +0200

    1 change(s) from matias@karhu

--- edit_chezmoi_cfg_files.sh
+++ edit_chezmoi_cfg_files.sh
@@ -17,27 +17,6 @@ preview_command() {
     fi
 }
 export -f preview_command
-# colorize_file_list() {
-#     # ANSI color codes
-#     RESET="\033[0m"
-#     FOLDER="\033[34m"
-#     BASE="\033[36m"
-#     FILE="\033[35m"
-#
-#     while IFS= read -r line; do
-#         if [ -d "$HOME/$line" ]; then
-#             # Directories are bold blue
-#             echo -e "${FOLDER}${line}/${RESET}"
-#         else
-#             base_name="$(basename "$line")"
-#             dir_name="$(dirname "$line")/"
-#             if [ "$dir_name" = "./" ]; then
-#                 dir_name=''
-#             fi
-#             echo -e "${BASE}${dir_name}${RESET}${FILE}${base_name}${RESET}"
-#         fi
-#     done
-# }
 
 search_files() {
     chezmoi managed --include=files |
