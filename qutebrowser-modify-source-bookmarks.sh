de23678fb46e09b6d2dd229025cfdc88b19aefdf
Matias Ceau <matias@ceau.net>
2024-09-18 19:42:51 +0200

    1 change(s) from matias@karjala

--- qutebrowser-modify-source-bookmarks.sh
+++ qutebrowser-modify-source-bookmarks.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 local_book="$XDG_CONFIG_HOME/qutebrowser/bookmarks/urls"
-chezm_book_rel="dot_config/qutebrowser/bookmarks/urls"
-chezm_book="$CHEZMOI/$chezm_book_rel"
+chezm_book="$CHEZMOI/dot_config/qutebrowser/bookmarks/urls"
 
 local_quick="$XDG_CONFIG_HOME/qutebrowser/quickmarks"
-chezm_quick_rel="dot_config/qutebrowser/quickmarks"
-chezm_quick="$CHEZMOI/$chezm_quick_rel"
+chezm_quick="$CHEZMOI/dot_config/qutebrowser/quickmarks"
 
+local_sessions="$XDG_DATA_HOME/qutebrowser/sessions"
+chezm_sessions="$CHEZMOI/dot_local/share/private_qutebrowser/sessions"
 # wait in case it's a quickmark and the name needs to be added
 sleep 20 && \
+    cat "$local_quick" > "$chezm_quick"
 cat "$local_book" > "$chezm_book"
-cat "$local_quick" > "$chezm_quick"
 
+for f in $(fd '.yml$' -tf --format '{/}' "$local_sessions"); do
+    cat "$local_sessions/$f" > "$chezm_sessions/$f"
+done
 # chezmoi cd && \
-# git add "$chezm_book_rel" && \
-# git add "$chezm_quick_rel" && \
-# git commit -m "Updated bookmarks ($(date +%Y-%m-%d_%H-%M))" && \
-# git push
+    # git add "$chezm_book_rel" && \
+    # git add "$chezm_quick_rel" && \
+    # git commit -m "Updated bookmarks ($(date +%Y-%m-%d_%H-%M))" && \
+    # git push
 
