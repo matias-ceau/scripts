b111516b4cdad34111d8c66aec3f965f298adc78
Matias Ceau <matias@ceau.net>
2024-09-24 12:12:50 +0200

    2 change(s) from matias@karjala

--- obsidian-vault-selector.sh
+++ obsidian-vault-selector.sh
@@ -5,17 +5,19 @@
 SHELL="$(which bash)"
 
 preview_cmd() {
+    eza -la --list-dirs --time=modified --no-permissions --time-style=long-iso --total-size --color=always --icons=always "$1"
     eza \
         -T \
         --sort=modified \
         --color=always \
         --icons=always \
-        "$HOME/PKM/$1" |
-        rg -v '\.js|\.css|\.png|\.gif|\.svg|\.zip'
+        "$1" |
+    rg -v '\.js|\.css|\.png|\.gif|\.svg|\.zip' | bat -r 2: -pp --color=always
 }
 
 open_vault() {
     if [ -n "$1" ]; then
+        vault_name="$(echo "$1" | awk -F/ '{print $(NF)}')"
         cmd="obsidian obsidian://open?vault=$1"
         setsid bash -c "$cmd" &
     else
@@ -23,20 +25,36 @@ open_vault() {
     fi
 }
 
+search_cmd() {
+    cat $XDG_CONFIG_HOME/obsidian/obsidian.json |
+    jq '.vaults .[] .path' | sed 's/"//g' |
+    eza --oneline --color=always --icons=never --stdin --list-dirs
+}
+# fd -td -Hg '.obsidian' "$HOME/PKM" -E ".debris" awk -F/ '{print $(NF-2)}'
+
+plabel_cmd() {
+    eza --oneline --list-dirs --color=always --icons=always "$1"
+}
+
 export -f preview_cmd
 export -f open_vault
+export -f search_cmd
+export -f plabel_cmd
 
-vault="$(fd -td \
-    -Hg \
-    '.obsidian' \
-    "$HOME/PKM" |
-    awk -F/ '{print $(NF-2)}' |
+vault="$(search_cmd |
     improved-fzfmenu.sh  \
         title_is_obsidian-open-vault \
         --pipe \
-        --preview-window='70%' \
+        --with-nth -1 \
+        --delimiter / \
+        --ansi \
+        --border \
+        --info inline-right \
+        --preview-window='top,70%,border-bottom' \
         --bind="resize:refresh-preview" \
-        --preview-label="CONTENT" \
-        --preview='preview_cmd {}')"
+        --preview-label="" \
+        --preview-label-pos bottom \
+        --bind 'focus:transform-preview-label:plabel_cmd {1..-1}' \
+    --preview='preview_cmd {}')"
 
 open_vault "$vault"
--- setting-up-syncs.sh
+++ setting-up-syncs.sh
@@ -2,6 +2,7 @@
 
 HOST="$(hostnamectl hostname)"
 
+# XDG FOLDERS
 mega-sync "$HOME/Desktop" "/BACKUPS/$HOST/Desktop"
 mega-sync "$HOME/Downloads" "/BACKUPS/$HOST/Downloads"
 mega-sync "$HOME/Templates" "/BACKUPS/$HOST/Templates"
@@ -11,9 +12,26 @@ mega-sync "$HOME/Music" "/BACKUPS/$HOST/Music"
 mega-sync "$HOME/Pictures" "/BACKUPS/$HOST/Pictures"
 mega-sync "$HOME/Videos" "/BACKUPS/$HOST/Videos"
 
+# NOTES FOLDER
+mega-sync "$HOME/PKM" "/PKM"
+
 if [ "$HOST" = karhu ]; then
-    mega-sync "$HOME/PKM" "/PKM"
+    # Common Library
     mega-sync "$HOME/UnifiedLibrary" "/UnifiedLibrary"
     mega-sync "/mnt/mega/hauki/xiaomi_mega" "/HOST_QUICKSHARES/xiaomi_mega"
     mega-sync "/mnt/mega/karjala" "/BACKUPS/karjala"
+
+elif [ "$HOST" = karjala ]; then
+    # Common Library
+    mega-sync "/mnt/HDD2/MEGA/UnifiedLibrary" "/UnifiedLibrary"
+    # Media files too big to fit on every device
+    mega-sync "/mnt/HDD2/MEGA/AUDIOVISUAL" "/AUDIOVISUAL"
+    # Access to other hosts
+    mega-sync "/mnt/HDD2/devices/karhu" "/BACKUPS/karhu"
+    mega-sync "/mnt/HDD2/devices/hauki" "/BACKUPS/hauki"
+    mega-sync "/mnt/HDD2/devices/xiaomi_mega" "/HOST_QUICKSHARES/xiaomi_mega"
+    # TODO: also sync huge data folders:
+    # - ARCHIVES
+    # - DATA
+    # - LIMBO
 fi
