b6b24a3f399e4418f161b977f2570428576b1932
Matias Ceau <matias@ceau.net>
2024-09-29 07:39:00 +0200

    2 change(s) from matias@karjala

--- count_tokens_with_tiktoken.py
+++ count_tokens_with_tiktoken.py
@@ -1,19 +1,57 @@
 #! /usr/bin/env python
 
 import sys
+
 import tiktoken
 
-def count_tokens(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        text = file.read()
-    encoding = tiktoken.encoding_for_model("gpt-4o")
+
+def count_tokens(text, model):
+    encoding = tiktoken.encoding_for_model(model)
     tokens = encoding.encode(text)
     return len(tokens)
 
+
+def get_options(arguments):
+    text, model = None, "gpt-4o"
+    if len(arguments) != 1:
+        arguments = " ".join(arguments[1:])
+        if " -h " in arguments:
+            usage(0)
+        if " -f " in arguments:
+            try:
+                file_path = arguments.split("-f")[-1].split(" -")[0].strip()
+                with open(file_path, "r") as f:
+                    text = f.read()
+            except IndexError:
+                usage(1)
+        if " -m " in arguments:
+            try:
+                model = arguments.split("-m")[-1].split(" -")[0].strip()
+            except IndexError:
+                usage(1)
+    return text, model
+
+
+def usage(exit_code=1):
+    USAGE = """\
+Usage:
+    <cmd> | ./count_tokens.py 
+    ./count_tokens_with_tiktoken.py -f <file_path>
+
+Options:
+    -h -- help
+    -m -- model (default: gpt-4o)
+"""
+    print(USAGE)
+    sys.exit(exit_code)
+
+
+def main(arg):
+    text, model = get_options(arg)
+    text = text if text else sys.stdin.read()
+    num_tokens = count_tokens(text, model)
+    print(f"nb_tokens\t{num_tokens}")
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python count_tokens.py <file_path>")
-        sys.exit(1)
-    file_path = sys.argv[1]
-    num_tokens = count_tokens(file_path)
-    print(f"Number of tokens: {num_tokens}")
+    main(sys.argv)
---
+++ tui-gpt.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "llm",
+#     "textual",
+# ]
+# ///
+import os
+
+from openai import OpenAI
+from rich.terminal_theme import MONOKAI
+from textual import on, work
+from textual.app import App, ComposeResult
+from textual.containers import VerticalScroll
+from textual.reactive import Reactive
+from textual.widgets import Footer, Header, Input, Markdown
+
+SYSTEM = """Formulate all responses as if you where the sentient AI named Mother from the Aliens movies."""
+MODEL = "gpt-4o-2024-08-06"
+
+
+class Prompt(Markdown):
+    pass
+
+
+class Response(Markdown):
+    BORDER_TITLE = MODEL
+
+
+class MotherApp(App):
+    ansi_theme_dark = Reactive(MONOKAI, init=False)
+    AUTO_FOCUS = "Input"
+
+    CSS = """
+    Prompt {
+        background: $primary 10%;
+        color: $text;
+        margin: 1;        
+        margin-right: 8;
+        padding: 1 2 0 2;
+    }
+
+    Response {
+        border: wide $success;
+        background: $success 10%;   
+        color: $text;             
+        margin: 1;      
+        margin-left: 8; 
+        padding: 1 2 0 2;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        with VerticalScroll(id="chat-view"):
+            yield Response("INTERFACE 2037 READY FOR INQUIRY")
+        yield Input(placeholder="How can I help you?")
+        yield Footer()
+
+    def on_mount(self) -> None:
+        # self.model = llm.get_model("gpt-4o-2024-08-06")
+        self.model = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+    @on(Input.Submitted)
+    async def on_input(self, event: Input.Submitted) -> None:
+        chat_view = self.query_one("#chat-view")
+        event.input.clear()
+        await chat_view.mount(Prompt(event.value))
+        await chat_view.mount(response := Response())
+        response.anchor()
+        self.send_prompt(event.value, response)
+
+    @work(thread=True)
+    def send_prompt(self, prompt: str, response: Response) -> None:
+        response_content = ""
+        # llm_response = self.model.prompt(prompt, system=SYSTEM)
+        llm_response = self.model.chat.completions.create(
+            model=MODEL,
+            stream=True,
+            messages=[
+                {"role": "system", "content": SYSTEM},
+                {"role": "user", "content": prompt},
+            ],
+        )
+        for chunk in llm_response:
+            response_content += chunk.choices[0].delta.content or ""
+            self.call_from_thread(response.update, response_content)
+
+
+if __name__ == "__main__":
+    app = MotherApp()
+    app.run()
