42814e8a696329f91691adfb9f155c2eb45ed9b2
Matias Ceau <matias@ceau.net>
2025-08-20 19:00:05 +0200

    1 change(s) from matias@None

--- bin/sync-repo.py
+++ bin/sync-repo.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S uv run --script --quiet
+#
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "rich",
+# ]
+# ///
+
 
 import argparse
 import datetime
